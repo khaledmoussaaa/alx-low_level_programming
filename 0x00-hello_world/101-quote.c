657997935977148e9f400508729e6d2aa7b1d8e6
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-10 19:50:29 -0700

    cccc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
@@ -7,10 +6,7 @@
  *
  * Return: Always 1 (Failed)
 */
-
  int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
