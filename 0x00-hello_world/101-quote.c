80288c97f40696da68cd0cb96bb4cdecca73d217
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-10 19:52:07 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,9 @@
  *
  * Return: Always 1 (Failed)
 */
- int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
