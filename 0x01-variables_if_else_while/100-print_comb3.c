1648e81cd44c302358cd4060daf4957dc26e28be
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-11 18:41:42 -0700

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: check lowercase
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
