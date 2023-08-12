fe6e874b981c5e3b2c2778d04127cfcbf444848e
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-11 19:35:48 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,26 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = 0; j <= 99; j++)
 		{
-			putchar('0' + i / 10);
-			putchar('0' + i % 10);
-			putchar(' ');
+			int i1 = i / 10;
+			int i2 = i % 10;
+			int j1 = j / 10;
+			int j2 = j % 10;
+
+			if (i <= j)
+			{
+				putchar(i1 + '0');
+				putchar(i2 + '0');
+				putchar(' ');
+				putchar(j1 + '0');
+				putchar(j2 + '0');
+				if (i != 99 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	return (0);
