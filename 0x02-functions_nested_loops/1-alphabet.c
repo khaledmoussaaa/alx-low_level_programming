106152959fa88b56c29abe8a8687e992dcadf527
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-15 11:41:57 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar([ch]);
+	_putchar('\n');
+	return (0);
+}
