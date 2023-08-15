44860da0b240309c7ebed80e6b0c23b6f307720e
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-15 09:11:30 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print _pucthcar in new line
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar \n");
+
+	return (0);
+}
