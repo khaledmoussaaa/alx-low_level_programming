f474e2a4ab0c8888918ae92a43df57013b0f6212
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-16 18:01:46 -0700

    comit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: get fibonacci for 50
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        float evenSum;
+        unsigned long f1 = 0;
+        unsigned long f2 = 1;
+        unsigned long sum;
+
+	while (1)
+	{
+		sum = f1 + f2;
+		if (sum > 4000000)
+		       break;	
+
+                if (sum % 2 == 0)
+			evenSum = evenSum + sum;
+		f1 = f2;
+		f2 = sum;
+        }
+	printf("ul\n", evenSum);
+        return (0);
+}
