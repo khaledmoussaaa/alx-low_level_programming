881eb43fdf8b61a6b5b116e93da6b90988911ea5
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-16 18:07:34 -0700

    com

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,22 +9,22 @@
  */
 int main(void)
 {
-        float evenSum;
-        unsigned long f1 = 0;
-        unsigned long f2 = 1;
-        unsigned long sum;
+	float evenSum;
+	unsigned long f1 = 0;
+	unsigned long f2 = 1;
+	unsigned long sum;
 
 	while (1)
 	{
 		sum = f1 + f2;
 		if (sum > 4000000)
-		       break;	
+			break;
 
-                if (sum % 2 == 0)
+		if (sum % 2 == 0)
 			evenSum = evenSum + sum;
 		f1 = f2;
 		f2 = sum;
-        }
+	}
 	printf("%.0f\n", evenSum);
-        return (0);
+	return (0);
 }
