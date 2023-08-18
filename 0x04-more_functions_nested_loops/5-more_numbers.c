292060a1734f003e8c9f0710bf9e94974e071637
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-17 19:45:24 -0700

    com

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,14 +3,14 @@
 /**
  * more_numbers - prints num from 0 to 9
  *
- * Return: 1
+ * Return: 0
  */
 
 void more_numbers(void)
 {
 	int i, j, num;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
