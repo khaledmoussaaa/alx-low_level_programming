e88768589667ff35ecb368f83732d440cfcef637
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-21 13:56:36 -0700

    comit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - takes a pinter and update to 98 integer
+ *
+ *@s: input function
+ *
+ * Return: null
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+}
