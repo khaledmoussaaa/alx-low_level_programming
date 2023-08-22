34cd8b278813f26c132c74774ce7dbb5d94c66ee
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-21 18:55:09 -0700

    com

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int pass;
+	char c;
+
+	strand(time(NULL));
+	while (pass <= 2645)
+	{
+		c = rand() % 128;
+		pass = pass + c;
+		putchar(c);
+	}
+	putchar(2772 - pass);
+	return (0);
+}
+
