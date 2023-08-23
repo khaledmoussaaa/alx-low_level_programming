a908c68a734dc8bbd25b406e1ba809d61fd97074
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-23 15:43:16 -0700

    com

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int result;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
@@ -22,5 +22,5 @@ int _strcmp(char *s1, char *s2)
 		}
 		i++;
 	}
-	return (0);
+	return (result);
 }
