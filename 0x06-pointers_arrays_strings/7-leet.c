f73a3d80e64d63762012bbcdde5ebd7cd984ed9f
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-23 19:23:36 -0700

    com

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - capitalizes all words of a string.
+ *
+ * @ch: input fucntion
+ *
+ * Return: pointer
+ */
+char *leet(char *ch);
+{
+	int i, j;
+	char decode[] = "aAeEoOtTlL";
+	char encode[] = "4433007711";
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (ch[i] == decode[j])
+			{
+				ch[i] = encode[j];
+			}
+		}
+	}
+	return (ch);
+}
