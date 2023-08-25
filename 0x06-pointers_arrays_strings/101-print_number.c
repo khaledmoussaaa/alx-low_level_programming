c38fc133703bdaee92427ffbd04f56e071fbc5ed
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-25 11:55:11 -0700

    com

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print an integers numbers
+ *
+ * @n: input funciton
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_numbers(n1 / 10);
+	}
+	putchar((n1 % 10) + 48);
+}
+
