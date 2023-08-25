5fafefe39ae4ec23cafea950060c1cda4186bcbe
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-25 11:57:17 -0700

    com

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 	n1 = n;
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
-		print_numbers(n1 / 10);
+		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + 48);
+	_putchar((n1 % 10) + 48);
 }
 
