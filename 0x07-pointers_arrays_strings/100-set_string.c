276597d510feb234f543f6ba8ef292bb5ed071a9
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-28 18:30:15 -0700

    com

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*print_diagsums - sets the value of a pointer to a char.
+*@s: input fnction
+*@to: input fnction
+*
+*Return: zero
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
