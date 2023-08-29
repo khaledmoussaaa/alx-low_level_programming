7e25debb8784b4b237c54a18ad70757de570b521
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-28 18:20:57 -0700

    com

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_diagsums - prints the sum of the two diagonals of matrix.
+*@a: input fnction
+*@size: input fnction
+*
+*Return: zero
+*/
+void print_diagsums(int *a, int size);
+{
+	int i, d1, d2;
+
+	for (i = 0; i < size; i++)
+	{
+		d1 = d1 + a[i];
+		d2 = d2 + a[size - i - 1];
+		a = a + size;
+	}
+	printf("%d, ", d1);
+	printf("%d \n", d2);
+}
