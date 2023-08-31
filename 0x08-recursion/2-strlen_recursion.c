e67df743274384f05e0f0d401c2b349b67e60836
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-08-30 17:15:53 -0700

    com

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,6 +11,6 @@ int _strlen_recursion(char *s)
 	int count = 0;
 
 	if (*s != '\0')
-		count = count _strlen_recursion(s + 1) + 1;
+		count = count + _strlen_recursion(s + 1) + 1;
 	return (count);
 }
