610cc521f8df9be88a925e8e505d25348fb4eaf9
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-05 18:42:31 -0700

    com

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+/**
+ * countWords - count string
+ * @str: input function
+ * Return: integer
+ */
+int countWords(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				count++;
+		}
+		else if (i == 0)
+			count++;
+	}
+	count++;
+	return (count);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: input function
+ * Return: count
+ */
+char **strtow(char *str)
+{
+	int i = 0, j, k, l, count = 0, we = 0;
+	char **mall;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	count = countWords(str);
+	if (count == 1)
+		return (NULL);
+	mall[count - 1] = NULL;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			mall[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(mall[k]);
+				free(mall[count - 1]);
+				free(mall);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				mall[wc][l] = str[i + l];
+			mall[wc][l] = '\0';
+			wc++;
+			i = i + j;
+		}
+		else
+			i++;
+	}
+	return (mall);
+}
