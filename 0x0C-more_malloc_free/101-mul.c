3dea4936ac860e1768e000eadd8edc567f80daf8
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-07 15:41:52 -0700

    com

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,26 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 
 /**
- * multiply - multiply two numbers
- * @num1: input function
- * @num2: input function
- * Return: integers
+*multiply - multiply two numbers
+*@num1: input function
+*@num2: input function
+*Return: integers
 */
-
 int multiply(int num1, int num2)
 {
 	return (num1 * num2);
 }
 /**
- * isNumber - check is number or not
- * @str: input function
- * Return: 1 or 0
+*isNumber - check is number or digit
+*@str: input function
+*Return: integers
 */
-
 int isNumber(char *str)
 {
 	while (*str)
@@ -32,31 +29,45 @@ int isNumber(char *str)
 	return (1);
 }
 /**
- * *main - print the result of multiply followed by new line
- * @argc: input function
- * @argv: input function
- * Return: 0
+*printError - print error message
 */
-
+void printError(void)
+{
+	printf("Error\n");
+}
+/**
+*printResult - print the result
+*@result: input function
+*/
+void printResult(int result)
+{
+	printf("%d\n", result);
+}
+/**
+*main - print multiplicaton numbers
+*@argc: input function
+*@argv: input function
+*Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n");
-		return (98);
+		printError();
+		exit(98);
 	}
 	char *num1Str = argv[1];
 	char *num2Str = argv[2];
 
 	if (!isNumber(num1Str) || !isNumber(num2Str))
 	{
-		printf("Error\n");
+		printError();
 		exit(98);
 	}
 	int num1 = atoi(num1Str);
 	int num2 = atoi(num2Str);
 	int result = multiply(num1, num2);
 
-	printf("%d\n", result);
+	printResult(result);
 	return (0);
 }
