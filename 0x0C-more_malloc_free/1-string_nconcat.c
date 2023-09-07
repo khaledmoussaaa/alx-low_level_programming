a20176509c93c46be0e1064409db07217f9f68b1
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-07 11:37:13 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,18 +1,5 @@
 #include "main.h"
-
-/**
- * getSize - get size of string
- * @s: input function
- * Return: integer
- */
-int getSize(char *s)
-{
-	int  i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+#include <stdio.h>
 /**
 *string_nconcat - concatenates two strings.
 *@s1: input function
@@ -22,22 +9,27 @@ int getSize(char *s)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j,  size1 = 0, size2 = 0;
+	unsigned int i, j, size1, size2;
 	char *conc;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	size1 = getSize(s1);
-	size2 = getSize(s2);
+	for (size1 = 0; s1[size1] != '\0' size1++)
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
 	conc = malloc(size1 + n + 1);
 	if (conc == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		conc[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n; j++)
+	{
 		conc[i] = s2[j];
+		i++;
+	}
 	conc[i] = '\0';
 	return (conc);
 }
