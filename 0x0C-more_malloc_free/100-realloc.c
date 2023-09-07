e5bee2df4137c0bfd9326f8348ab66340950239d
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-07 12:55:37 -0700

    Com

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*array_range - fcreallocates a memory block
+*@ptr: input function
+*@old_size: input function
+*@new_size: input function
+*Return: pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newPtr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		newPtr = malloc(newSize);
+		if (newPtr == NULL)
+			return (NULL);
+		return (newPtr);
+	}
+	if (new_size > old_size)
+	{
+		newPtr = malloc(new_size);
+		if (newPtr == NULL)
+			return (NULL);
+		for (i = 0; i < new_size && i < old_size; i++)
+			*((char *)newPtr + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (newPtr);
+}
