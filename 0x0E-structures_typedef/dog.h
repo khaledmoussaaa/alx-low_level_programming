6ccacb4e68fab963104ebb7364a8c3b400ab9dca
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-11 17:05:45 -0700

    com

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog for type of struct
+ * @name: input function
+ * @age: input function
+ * @owner: input function
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
