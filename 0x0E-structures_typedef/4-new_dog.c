2dc44f2a5a9aa04e9cb58987a9c3a7449a129f4e
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-11 18:17:08 -0700

    com

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - prototype
+ * @name: input function
+ * @age: input function
+ * @owner: input function
+ * Return: pointer
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = strcpy(dog->owner, owner);
+	return (dog);
+}
