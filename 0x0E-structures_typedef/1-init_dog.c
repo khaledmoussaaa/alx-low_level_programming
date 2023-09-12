6ebce3f5e9517abebea73470d6cd7360cde8cd69
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-11 17:39:02 -0700

    com

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include "doh.h"
+#include "dog.h"
 
 /**
  * init_dog - prototype
@@ -9,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
