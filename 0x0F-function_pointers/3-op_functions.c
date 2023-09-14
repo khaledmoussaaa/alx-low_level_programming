eace4e0660cc1e2b8fab3a94230b3026706138ff
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-09-13 19:29:05 -0700

    com

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+*op_add - searches for an integer.
+*@a: input function
+*@b: input function
+*Return: summation
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+*op_sub - searches for an integer.
+*@a: input function
+*@b: input function
+*Return: substraction
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+*op_mul - searches for an integer.
+*@a: input function
+*@b: input function
+*Return: multiplication
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+*op_div - searches for an integer.
+*@a: input function
+*@b: input function
+*Return: divation
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+*op_mod - searches for an integer.
+*@a: input function
+*@b: input function
+*Return: mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
