e30db93a93c47d653c4aae087dea3a8c52a26e11
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-10-02 17:17:43 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: can't read from file %s\n"
-#define ERR_NOWRITE "Error: can't write to %s\n"
-#define ERR_NOCLOSE "Error: can't close fd %d\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - program
  * @ac: argument count
  * @av: argument vector
- * Return: 1 || 0
+ * Return: 1 success 0 on fails
 */
 
 int main(int ac, char **av)
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 	int from_fd = 0, to_fd = 0;
 	ssize_t b;
 	char buf[READ_BUF_SIZE];
-	
+
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
@@ -31,7 +31,7 @@ int main(int ac, char **av)
 		if (write(to_fd, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 	if (from_fd)
