364787f74ba4dcffb01efffb0d0cc4e0ba36a55d
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-09 09:59:57 -0800

    t

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,15 +1,10 @@
-#include <unisted.h>
 #include "main.h"
-
 /**
- * _putchar - writer the chacr c
- * @c: The charachter to print
- *
- * Return: On success 1.
- *              On error, -1
+ * _putchar - Writes a single character.
+ * @ch: The character to write.
+ * Return: On success, returns 1. On failure, returns -1.
 */
-
-int _putchar(char c)
+int _putchar(char ch)
 {
-	return (write(1, &c, 1));
+	return (write(1, &ch, 1));
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Readme Pinter
+Readme
